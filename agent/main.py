bc9dd364a77e5b09b46b11a4ca4075fa047b4721
Enrico Z. Borba <enrico.borba@toptal.com>
2023-05-12 09:21:13 -0400

    write ort-detectable files to zip

---
+++ agent/main.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+from zipfile import ZipFile
+
+import click
+
+from agent.oss import Matcher
+
+
+@click.command()
+@click.option(
+    "-o",
+    "--output",
+    default="repository.zip",
+    metavar="FILE",
+    help="output zip filename",
+)
+@click.argument("repository")
+def main(repository, output):
+    """
+    Run local scan on REPOSITORY and generate a zip file.
+    """
+    matcher = Matcher()
+    paths = []
+
+    for root, _, files in os.walk(repository):
+        for filename in files:
+            if matcher.matches_any(filename):
+                rel_root = os.path.relpath(root, start=repository)
+                paths.append(os.path.join(rel_root, filename))
+
+    with ZipFile(output, "w") as zipfile:
+        for path in paths:
+            zipfile.write(os.path.join(repository, path), arcname=path)
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Agent
+
+## Usage
+Run the script using `poetry run agent`:
+```
+Usage: agent [OPTIONS] REPOSITORY
+
+  Run local scan on REPOSITORY.
+
+Options:
+  -o, --output TEXT  output zip filename
+  --help             Show this message and exit.
+```
