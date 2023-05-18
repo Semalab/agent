120d2d51d8691c1fd89c371343fe782b40b88bbb
Enrico Z. Borba <enrico.borba@toptal.com>
2023-05-18 13:48:22 -0400

    add ignore patterns

--- agent/strategy/scancode/__init__.py
+++ agent/strategy/scancode/__init__.py
@@ -2,11 +2,54 @@ import os
 import platform
 import subprocess
 
+from agent.utils import flatten
+
 
 class Scancode:
     """
     runs https://github.com/nexB/scancode-toolkit
     """
+
+    ARGS = [
+        "--processes",
+        "2",
+        "--only-findings",
+        "--timeout",
+        "120",
+        "--copyright",
+        "--package",
+        "--license",
+        "--license-score",
+        "10",
+    ]
+
+    IGNORE = [
+        "*.min.js",
+        "*.jpg",
+        "*.gif",
+        "*.bmp",
+        "*.tiff",
+        "*.psd",
+        "*.mp4",
+        "*.mkv",
+        "*.avi",
+        "*.mov",
+        "*.mpg",
+        "*.vob",
+        "*.mp3",
+        "*.aac",
+        "*.wav",
+        "*.flac",
+        "*.ogg",
+        "*.mka",
+        "*.wma",
+        "*.sql",
+        "*Debug0*",
+        "*sqlite*",
+        "*.png",
+        "*.dll",
+        "*.exe",
+    ]
 
     def __init__(self, scancode):
         self.bin_scancode = os.path.join(scancode, "scancode")
@@ -20,6 +63,8 @@ class Scancode:
 
         os.makedirs(os.path.join(directories.sema_output, "scancode"), exist_ok=True)
 
+        ignore = flatten([["--ignore", pattern] for pattern in Scancode.IGNORE])
+
         subprocess.run(
             [
                 bin,
@@ -27,9 +72,8 @@ class Scancode:
                 os.path.join(
                     directories.sema_output, "scancode", "scancode-output.csv"
                 ),
-                "--copyright",
-                "--license",
-                "--package",
+                *Scancode.ARGS,
+                *ignore,
                 directories.repository,
             ]
         )
---
+++ agent/utils.py
@@ -0,0 +1,2 @@
+def flatten(lst):
+    return [y for x in lst for y in x]
