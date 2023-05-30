3028144f3e6b5993dd6e07e2e892c9f6add5b5d4
Siddharth G <sghumre@sightmachine.com>
2023-05-30 21:30:58 +0530

    added support to detect repo type, vcs converion and dynamic output names

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import os
 import shutil
 import tempfile
 from zipfile import ZipFile
+from datetime import datetime
 
 import click
 from click import ClickException
 
 from agent.directories import Directories
 from agent.strategy import Dependencies, DependencyCheck, Scancode
+from agent.utils import detect_version_control_system
+from agent.vcs_converter import VCSConverter
 
 
 @click.command()
@@ -37,10 +40,17 @@ def main(repository, output, dependency_check, scancode):
     if not os.path.isdir(repository):
         raise ClickException(f"directory '{repository}' does not exist")
 
+    ts_now = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+
     repository = os.path.abspath(repository)
     output = os.path.abspath(output)
 
     with tempfile.TemporaryDirectory() as tempdir:
+        repo_type = detect_version_control_system(repository)
+        click.echo(f"Given repo is {repo_type} repo")
+        convertor = VCSConverter(repository, repo_type, tempdir)
+        repository = convertor.convert()
+
         strategies = [Dependencies()]
 
         if dependency_check:
@@ -54,12 +64,23 @@ def main(repository, output, dependency_check, scancode):
             strategy.run(directories)
 
         output_basename, output_ext = os.path.splitext(output)
+        output_basename = create_output_basename(output_basename, ts_now)
         output_ext = output_ext[1:]
         if not output_ext:
             click.echo("output has no extension, assuming .zip")
             output_ext = "zip"
 
         shutil.make_archive(output_basename, output_ext, tempdir)
+
+
+def create_output_basename(output_basename: str, ts_now: str) -> str:
+    """
+    Create the output zipfile name.
+    output format - "timestamp_reponame.zip"
+    """
+    zipname = output_basename.split("/")[-1]
+    new_name = output_basename.replace(zipname, f"{ts_now}_{zipname}")
+    return new_name
 
 
 if __name__ == "__main__":
