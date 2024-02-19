4278cb0aed5fb9d65032ead4b92f56f61a5d571a
farkaskid1 <siddharth@semasoftware.com>
2024-02-19 19:24:08 +0530

    sq-4830 -> Finishing touches

--- agent/strategy/commit_analysis/full_analysis.py
+++ agent/strategy/commit_analysis/full_analysis.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 class FullAnalysis:
-    userdir = '/home/siddharthshishulkar'
+    userdir = '/home/root'
     jar_name = 'backend-commitanalysis-1.0-SNAPSHOT-jar-with-dependencies.jar'
     basepath = f"{userdir}/.m2/repository/backend/backend-commitanalysis/1.0-SNAPSHOT"
 
@@ -12,7 +12,7 @@ class FullAnalysis:
         self.out = out_path
 
     def run(self, _):
-        out_path = Path(f"{self.out}/.sema/commit_analysis")
+        out_path = Path(self.out, '.sema', 'commit_analysis')
         try:
             Path.mkdir(out_path, parents=True) if not Path.exists(out_path) else None
             cmd = f"java -jar {self.basepath}/{self.jar_name} -agent -inputPath {self.repo} -outPath {out_path} -branch master"
